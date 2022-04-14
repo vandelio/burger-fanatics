e6063a0581b202f4b89d25d26d62d98362d71243
wesign <stefanvandel@gmail.com>
2022-04-14 22:22:41 +0200

    local git user name change succes

--- src/App.js
+++ src/App.js
@@ -11,44 +11,40 @@ import Reviews from "./pages/Reviews";
 import Spots from "./pages/Spots";
 import MyProfile from "./pages/MyProfile";
 
-
 import './styles/base.css';
 import './styles/transitions.css';
 import { CSSTransition, SwitchTransition } from 'react-transition-group';
 
-
 Amplify.configure(awsExports);
 function App() {
-  const [userComponent, setUserComponent] = useState(null)
-  const [activePageIndex, setActivePageIndex] = useState(0);
-  const [activePageName, setActivePageName] = useState(null);
+const [userComponent, setUserComponent] = useState(null)
+const [activePageIndex, setActivePageIndex] = useState(0);
+const [activePageName, setActivePageName] = useState(null);
 
-  const componentIndexToName = { 0: 'Home', 1: 'Reviews', 2: 'Spots', 3: 'MyProfile' }
-  const componentAssociator = { Home: Home, Reviews: Reviews, Spots: Spots, MyProfile: MyProfile }
+const componentIndexToName = { 0: 'Home', 1: 'Reviews', 2: 'Spots', 3: 'MyProfile' }
+const componentAssociator = { Home: Home, Reviews: Reviews, Spots: Spots, MyProfile: MyProfile }
 
-  useEffect(() => {
-    console.log('useEffect',componentIndexToName[activePageIndex]);
-    setActivePageName(componentIndexToName[activePageIndex])
-    setUserComponent(componentAssociator[componentIndexToName[activePageIndex]]);
-  },[activePageIndex])
+useEffect(() => {
+console.log('useEffect',componentIndexToName[activePageIndex]);
+setActivePageName(componentIndexToName[activePageIndex])
+setUserComponent(componentAssociator[componentIndexToName[activePageIndex]]);
+},[activePageIndex])
 
 if(userComponent === null || activePageName === null) return 'Loading...';
-  return (
-    <>
-      <SwitchTransition>
-        <CSSTransition
-          key={true}
-          addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}
-          classNames='fade'
-        >
-        <>
-          {userComponent} 
-        </>
-        </CSSTransition>
-      </SwitchTransition> 
-      <IconTabs activePageIndex={activePageIndex} setActivePageIndex={setActivePageIndex} />
-    </>
-  );
+return (
+<>
+  <SwitchTransition>
+    <CSSTransition key={true} addEndListener={(node, done)=> node.addEventListener("transitionend", done, false)}
+      classNames='fade'
+      >
+      <>
+        {userComponent}
+      </>
+    </CSSTransition>
+  </SwitchTransition>
+  <IconTabs activePageIndex={activePageIndex} setActivePageIndex={setActivePageIndex} />
+</>
+);
 }
 
 export default App;
--- src/pages/PageLayout.js
+++ src/pages/PageLayout.js
@@ -2,18 +2,19 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 function PageLayout(props) {
-  
-  return (
-    <>
-    <Box sx={{ width: '100%',height:'100%', display: 'flex', flexDirection:'column',borderBottom: 1, borderColor: 'divider' }}>
+
+return (
+<>
+    <Box
+        sx={{ width: '100%',height:'100%', display: 'flex', flexDirection:'column',borderBottom: 1, borderColor: 'divider' }}>
         <Box sx={{ p: 3 }}>
-          <Typography>Name:{props.name}</Typography>
-          children:
-          {props.children}
+            <Typography>Name:{props.name}</Typography>
+            children:
+            {props.children}
         </Box>
     </Box>
-    </>
-  );
+</>
+);
 }
 
 export default PageLayout;
