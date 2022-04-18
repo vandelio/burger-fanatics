faff41d444c6efc6cfee6414b8c3c391e6999ce5
wesign <stefanvandel@gmail.com>
2022-04-18 21:49:56 +0200

    profile console error fix

--- src/components/pages/MyProfile.js
+++ src/components/pages/MyProfile.js
@@ -9,24 +9,9 @@ function MyProfile({ signOut, user }) {
     <h3>Sign up to become one of us</h3>
   </PageLayout>;
 
-  const FeedTop = () => {
-    return (
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <h1>My Profile</h1>
-      </Box>
-    );
-  };
-
   return (
     <>
-      <PageLayout parts={FeedTop}>
+      <PageLayout>
         <>
           <h1>Hello {user.username}</h1>
           <button onClick={signOut}>Sign out</button>
